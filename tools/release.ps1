d2667629a3733498e37f85055e6bd9fffd8ee4ac
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2022-05-19 21:58:56 +0800

    a

--- tools/release.ps1
+++ tools/release.ps1
@@ -9,7 +9,7 @@ yarn
 Write-Host "Dependencies have been installed." -ForegroundColor Green
 ./tools/build.ps1
 
-$zip = "blessing-skin-server-6.0.0-rc-2.zip"
+$zip = "blessing-skin-server.zip"
 zip -9 -r $zip app bootstrap config database plugins public resources/lang resources/views resources/misc/textures routes storage vendor .env.example artisan LICENSE README.md README-zh.md index.html
 Write-Host "Zip archive is created." -ForegroundColor Green
 
